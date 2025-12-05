78c4de5c3210381a657d9ee1d24118ffc1100f77
Mahfuj-HSTU <rmahfujur16@gmail.com>
2025-12-05 21:48:54 +0600

    refactor: Modularize server by extracting configuration, database, and user routes into dedicated files and adding a logger middleware.

---
+++ src/config/db.ts
@@ -0,0 +1,35 @@
+import { Pool } from 'pg'
+import config from './index'
+
+export const pool = new Pool({
+  connectionString: config.database_url
+})
+
+export const initDb = async () => {
+  try {
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(100) UNIQUE NOT NULL,
+      age INT NOT NULL,
+      phone VARCHAR(15),
+      address TEXT,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+    )`)
+    await pool.query(`CREATE TABLE IF NOT EXISTS todos (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    is_completed BOOLEAN DEFAULT false,
+    due_date DATE,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+  )`)
+    console.log('Database connected')
+  } catch (error) {
+    console.error('Error initializing database:', error)
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,105 +1,23 @@
-import 'dotenv/config'
+import config from './config'
 import express, { Request, Response } from 'express'
-import { Pool } from 'pg'
+import { initDb, pool } from './config/db'
+import logger from './middleware/logger'
+import { userRoutes } from './modules/user/user.routes'
 const app = express()
-const port = 5000
+const port = config.port
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL?.includes('localhost')
-    ? false
-    : {
-        rejectUnauthorized: false
-      },
-  // increase timeouts to avoid transient ETIMEDOUT
-  connectionTimeoutMillis: 20000,
-  idleTimeoutMillis: 30000,
-  max: 5
-})
-
-const initDb = async () => {
-  try {
-    await pool.query(`
-    CREATE TABLE IF NOT EXISTS users (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(100) NOT NULL,
-      email VARCHAR(100) UNIQUE NOT NULL,
-      age INT NOT NULL,
-      phone VARCHAR(15),
-      address TEXT,
-      created_at TIMESTAMP DEFAULT NOW(),
-      updated_at TIMESTAMP DEFAULT NOW()
-    )`)
-    await pool.query(`CREATE TABLE IF NOT EXISTS todos (
-    id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE,
-    title VARCHAR(100) NOT NULL,
-    description TEXT,
-    is_completed BOOLEAN DEFAULT false,
-    due_date DATE,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-  )`)
-    console.log('Database connected')
-  } catch (error) {
-    console.error('Error initializing database:', error)
-  }
-}
-
+//* initial db
 initDb()
 
 // * parsing json body middleware
 app.use(express.json())
 // app.use(express.urlencoded({ extended: true })) //? for form data
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', logger, (req: Request, res: Response) => {
   res.send('Hello World! with typescript')
 })
 
-// *post users
-app.post('/users', async (req: Request, res: Response) => {
-  const { name, email, age } = req.body
-  try {
-    const result = await pool.query(
-      `INSERT INTO users (name, email, age) VALUES ($1, $2, $3)`,
-      [name, email, age]
-    )
-    res.send({
-      success: true,
-      message: 'User created successfully',
-      data: result.rows[0]
-    })
-  } catch (error: any) {
-    res.status(500).send({
-      success: false,
-      message: 'Failed to create user',
-      error: error.message
-    })
-  }
-  res.send({
-    success: true,
-    message: 'Data received successfully',
-    data: req.body
-  })
-})
-
-// *get users
-app.get('/users', async (req: Request, res: Response) => {
-  try {
-    const result = await pool.query(`SELECT * FROM users`)
-    res.send({
-      success: true,
-      message: 'Users fetched successfully',
-      data: result.rows
-    })
-  } catch (error: any) {
-    res.status(500).send({
-      success: false,
-      message: 'Failed to fetch users',
-      error: error.message
-    })
-  }
-})
+app.use('/users', userRoutes)
 
 // *get user by id
 app.get('/users/:id', async (req: Request, res: Response) => {
