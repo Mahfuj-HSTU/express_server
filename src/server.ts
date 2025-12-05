527f46e5c1e99f14938466a6d78c97b23d806439
Mahfuj-HSTU <rmahfujur16@gmail.com>
2025-12-05 10:08:05 +0600

    create and get users

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ const initDb = async () => {
     CREATE TABLE IF NOT EXISTS users (
       id SERIAL PRIMARY KEY,
       name VARCHAR(100) NOT NULL,
-      email VARCHAR(100) NOT NULL,
+      email VARCHAR(100) UNIQUE NOT NULL,
       age INT NOT NULL,
       phone VARCHAR(15),
       address TEXT,
@@ -56,13 +56,76 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World! with typescript')
 })
 
-app.post('/', (req: Request, res: Response) => {
-  console.log(req.body)
+// *post users
+app.post('/users', async (req: Request, res: Response) => {
+  const { name, email, age } = req.body
+  try {
+    const result = await pool.query(
+      `INSERT INTO users (name, email, age) VALUES ($1, $2, $3)`,
+      [name, email, age]
+    )
+    res.send({
+      success: true,
+      message: 'User created successfully',
+      data: result.rows[0]
+    })
+  } catch (error: any) {
+    res.status(500).send({
+      success: false,
+      message: 'Failed to create user',
+      error: error.message
+    })
+  }
   res.send({
     success: true,
     message: 'Data received successfully',
     data: req.body
   })
+})
+
+// *get users
+app.get('/users', async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users`)
+    res.send({
+      success: true,
+      message: 'Users fetched successfully',
+      data: result.rows
+    })
+  } catch (error: any) {
+    res.status(500).send({
+      success: false,
+      message: 'Failed to fetch users',
+      error: error.message
+    })
+  }
+})
+
+// *get user by id
+app.get('/users/:id', async (req: Request, res: Response) => {
+  const { id } = req.params
+  try {
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [id])
+    if (result.rows.length === 0) {
+      return res.status(404).send({
+        success: false,
+        message: 'User not found',
+        error: 'User not found'
+      })
+    } else {
+      res.send({
+        success: true,
+        message: 'User fetched successfully',
+        data: result.rows[0]
+      })
+    }
+  } catch (error: any) {
+    res.status(500).send({
+      success: false,
+      message: 'Failed to fetch user',
+      error: error.message
+    })
+  }
 })
 
 app.listen(port, () => {
